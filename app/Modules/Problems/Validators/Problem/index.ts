316e4a53cbae8e7c7597097ef45e6a727bff84f8
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-12-05 20:17:38 -0300

    refactor: remove escape in store validator

--- app/Modules/Problems/Validators/Problem/index.ts
+++ app/Modules/Problems/Validators/Problem/index.ts
@@ -2,7 +2,7 @@ import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
 export const StoreProblemSchema = schema.create({
   title: schema.string({ trim: true, escape: true }, []),
-  body: schema.string({ trim: true, escape: true }, []),
+  body: schema.string({ trim: true }, []),
   language: schema.string({ trim: true, escape: true }, []),
   owner_id: schema.string.optional({ trim: true, escape: true }, [
     rules.exists({ table: 'users', column: 'id', whereNot: { is_deleted: true } }),
@@ -11,7 +11,7 @@ export const StoreProblemSchema = schema.create({
 
 export const EditProblemSchema = schema.create({
   title: schema.string.optional({ trim: true, escape: true }, []),
-  body: schema.string.optional({ trim: true, escape: true }, []),
+  body: schema.string.optional({ trim: true }, []),
   language: schema.string.optional({ trim: true, escape: true }, []),
   owner_id: schema.string.optional({ trim: true, escape: true }, [
     rules.exists({ table: 'users', column: 'id', whereNot: { is_deleted: true } }),
